bc1f4eac692a9aabcc359af3dedc714be2eeee65
Karan Singhal <karan1149@gmail.com>
2018-05-28 12:12:31 -0700

    updated utils

--- unit5/utils.py
+++ unit5/utils.py
@@ -9,8 +9,9 @@ from collections import defaultdict
 
 def get_X_y(text):
 	text = text.lower()
-
+	print(len(text))
 	text = simplify_text(text)
+	print(len(text))
 
 	print('Corpus length:', len(text))
 
@@ -36,7 +37,7 @@ def get_X_y(text):
 	    for t, char in enumerate(sentence):
 	        x[i, t, char_indices[char]] = 1
 	    y[i, char_indices[next_chars[i]]] = 1
-	return x, y
+	return x, y, char_indices, indices_char
 
 def simplify_text(text):
     counts = defaultdict(int)
@@ -49,3 +50,40 @@ def simplify_text(text):
             text = text.replace(ch, '')
             removed += 1
     return text
+
+def sample_from_model(model, text, char_indices, indices_char, chunk_length, number_of_characters):
+	text = text.lower()
+	start_index = random.randint(0, len(text) - chunk_length - 1)
+	for diversity in [0.2, 0.5, 0.7]:
+	    print('----- diversity:', diversity)
+
+	    generated = ''
+	    sentence = text[start_index: start_index + chunk_length]
+	    generated += sentence
+	    print('----- Generating with seed: "' + sentence + '"')
+	    sys.stdout.write(generated)
+
+	    for i in range(400):
+	        x_pred = np.zeros((1, chunk_length, number_of_characters))
+	        for t, char in enumerate(sentence):
+	            x_pred[0, t, char_indices[char]] = 1.
+
+	        preds = model.predict(x_pred, verbose=0)[0]
+	        next_index = sample(preds, diversity)
+	        next_char = indices_char[next_index]
+
+	        generated += next_char
+	        sentence = sentence[1:] + next_char
+
+	        sys.stdout.write(next_char)
+	        sys.stdout.flush()
+	    print("\n")
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
